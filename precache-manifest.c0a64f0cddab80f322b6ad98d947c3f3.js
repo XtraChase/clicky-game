087cb8ae79638433d6c3faf6eccd117ce2d51c5d
Chase Laptop <james.chase.richards@gmail.com>
2020-01-12 14:57:15 -0500

    Updates

---
+++ precache-manifest.c0a64f0cddab80f322b6ad98d947c3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c65a1e9aafb61d4496bad38b84a2f93d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9b6f60468adea0a6b8d0",
+    "url": "/clicky-game/static/css/main.5213acd9.chunk.css"
+  },
+  {
+    "revision": "f28b742c844f6f4ac46e",
+    "url": "/clicky-game/static/js/2.2ca7913f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.2ca7913f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b6f60468adea0a6b8d0",
+    "url": "/clicky-game/static/js/main.57c73efb.chunk.js"
+  },
+  {
+    "revision": "68a16d1f51c6d00a3f7d",
+    "url": "/clicky-game/static/js/runtime-main.afb41716.js"
+  }
+]);
