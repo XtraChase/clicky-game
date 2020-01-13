906f00f6d234a3cc2be660856323543c42e517c6
Chase Laptop <james.chase.richards@gmail.com>
2020-01-13 11:38:55 -0500

    added emojis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import Emojis from "./components/Emojis";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Counter from "./components/Counter";
-import friends from "./friends.json";
+import emoji from "./emoji.json";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.emoji to the emoji json array
   state = {
-    friends,
+    emoji,
     count: 0
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+  removeEmoji = id => {
+    // Filter this.state.emoji for emoji with an id not equal to the id being removed
+    const emoji = this.state.emoji.filter(emoji => emoji.id !== id);
+    // Set this.state.emoji equal to the new emoji array
+    this.setState({ emoji });
   };
 
   // handleIncrement increases this.state.count by 1
@@ -25,22 +25,22 @@ class App extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  // Map over this.state.emoji and render a Emojis component for each emoji object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <Title>Emoji Game</Title>
         <Counter />
         {/* <div className="card-body">
           <p className="card-text">Count: {this.count}</p>
         </div> */}
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
+        {this.state.emoji.map(emoji => (
+          <Emojis
+            removeemoji={this.removeEmoji}
+            id={emoji.id}
+            key={emoji.id}
+            name={emoji.name}
+            image={emoji.image}
             handleIncrement={this.handleIncrement}
             onClick={this.handleIncrement}
           />
