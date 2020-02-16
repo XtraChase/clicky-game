fb551b0e5297a47dd2b12df8d1dd8c36e3e9a265
Chase Laptop <james.chase.richards@gmail.com>
2020-02-15 19:23:08 -0500

    stashed changes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,45 @@ class App extends Component {
   state = {
     emoji,
     count: 0,
+<<<<<<< HEAD
     highScore: 0
   };
+=======
+    clicked: false
+  };
 
+  addToUsedEmojis = id => {
+    // Filter this.state.emoji for emoji with an id not equal to the id being removed
+    const emoji = this.state.emoji.filter(emoji => emoji.id !== id);
+    // Set this.state.emoji equal to the new emoji array
+    this.setState({ emoji });
+  };
+
+  shuffleEmojis = emoji => {
+    let i = emoji.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = emoji[i];
+      emoji[i] = emoji[j];
+      emoji[j] = temp;
+    }
+    return emoji;
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  // handleIncrement increases this.state.count by 1
+  handleIncrement() {
+    console.log("hello");
+    // We always use the setState method to update a component's state
+    this.setState(state => ({ count: this.state.count + 1 }));
+  }
+>>>>>>> parent of d1815dd... emojis now shuffle
+
+  // Map over this.state.emoji and render a Emojis component for each emoji object
   render() {
     return (
       <Wrapper>
@@ -25,7 +61,23 @@ class App extends Component {
         </SubTitle>
 
         <div className="phone-container">
+<<<<<<< HEAD
           <Phone score={this.state.highScore} />
+=======
+          <Phone />
+        </div>
+        <div className="emoji-container">
+          {this.state.emoji.map(emoji => (
+            <Emojis
+              id={emoji.id}
+              key={emoji.id}
+              name={emoji.name}
+              image={emoji.image}
+              // handleIncrement={this.handleIncrement}
+              onClick={this.handleIncrement}
+            />
+          ))}
+>>>>>>> parent of d1815dd... emojis now shuffle
         </div>
         <Emojis />
       </Wrapper>
--- src/components/Phone/index.js
+++ src/components/Phone/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Countdown from "react-countdown-now";
 import "./style.css";
 
@@ -6,8 +6,11 @@ const Completionist = () => <span>Times Up!</span>;
 
 const renderer = ({ seconds, completed }) => {
   if (completed) {
+<<<<<<< HEAD
     console.log("Times Up!");
     // window.location.reload();
+=======
+>>>>>>> parent of d1815dd... emojis now shuffle
     // Render a completed state
     return <Completionist />;
   } else {
@@ -17,9 +20,19 @@ const renderer = ({ seconds, completed }) => {
 };
 
 // By extending the React.Component class, Phoneinherits functionality from it
+<<<<<<< HEAD
 class Phone extends Component {
   // constructor(props) {
   //   super(props);
+=======
+class Phone extends React.Component {
+  // Setting the initial state of the Phone
+  component;
+  state = {
+    count: 0,
+    clicked: false
+  };
+>>>>>>> parent of d1815dd... emojis now shuffle
 
   // The render method returns the JSX that should be rendered
   render(props) {
@@ -28,13 +41,14 @@ class Phone extends Component {
     return (
       <div className="card text-center smartphone">
         <div className="content">
+<<<<<<< HEAD
           <div className="high-score">High Score: {this.props.score}</div>
+=======
+          <div className="high-score">High Score: {this.state.count}</div>
+>>>>>>> parent of d1815dd... emojis now shuffle
           <div className="countdown">
             <Countdown date={Date.now() + 30000} renderer={renderer} />
           </div>
-          <br />
-          <div className="speech-bubble">Hi! How's it going?</div>
-          {/* <div className="reply-speech-bubble">...</div> */}
         </div>
       </div>
     );
